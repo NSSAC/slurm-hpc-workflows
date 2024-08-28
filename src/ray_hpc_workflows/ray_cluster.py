f967d508f7ec8bc5de500caa21e53aba49e602f2
Parantapa Bhattacharya <pb@parantapa.net>
2024-08-28 09:27:47 -0400

    Add simple map and apply helpers

--- src/ray_hpc_workflows/ray_cluster.py
+++ src/ray_hpc_workflows/ray_cluster.py
@@ -16,6 +16,7 @@ from collections import defaultdict
 
 import ray
 import platformdirs
+from more_itertools import chunked
 from pydantic import BaseModel
 
 
@@ -377,3 +378,53 @@ class RayCluster(Closeable):
 
         if self.temp_dir.exists():
             shutil.rmtree(self.temp_dir)
+
+
+@ray.remote
+def _ray_map_kwargs_chunk(func, kwargs_list_chunk):
+    results = [func(**kwargs) for kwargs in kwargs_list_chunk]
+    return results
+
+
+def ray_map_kwargs(
+    func,
+    kwargs_list: list[dict],
+    chunksize: int,
+    **ray_options_kwargs,
+):
+    ray_map_kwargs_chunk = _ray_map_kwargs_chunk.options(**ray_options_kwargs)  # type: ignore
+
+    kwargs_list_chunks = list(chunked(kwargs_list, chunksize))
+    tasks = [
+        ray_map_kwargs_chunk.remote(func, kwargs_list_chunk)
+        for kwargs_list_chunk in kwargs_list_chunks
+    ]
+    results = ray.get(tasks)
+
+    ret = []
+    for result in results:
+        ret.extend(result)
+
+    return ret
+
+
+@ray.remote
+def _ray_apply_kwargs_chunk(func, kwargs_list_chunk):
+    for kwargs in kwargs_list_chunk:
+        func(**kwargs)
+
+
+def ray_apply_kwargs(
+    func,
+    kwargs_list: list[dict],
+    chunksize: int,
+    **ray_options_kwargs,
+):
+    ray_apply_kwargs_chunk = _ray_apply_kwargs_chunk.options(**ray_options_kwargs)  # type: ignore
+
+    kwargs_list_chunks = list(chunked(kwargs_list, chunksize))
+    tasks = [
+        ray_apply_kwargs_chunk.remote(func, kwargs_list_chunk)
+        for kwargs_list_chunk in kwargs_list_chunks
+    ]
+    _ = ray.get(tasks)
