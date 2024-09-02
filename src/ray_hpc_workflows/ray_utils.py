0c260933a1bd0e9865aee979a3d5f4c4349043cb
Parantapa Bhattacharya <pb@parantapa.net>
2024-09-02 18:36:11 -0400

    Add chunksize computation support in ray_map/ray_apply

--- src/ray_hpc_workflows/ray_utils.py
+++ src/ray_hpc_workflows/ray_utils.py
@@ -1,5 +1,7 @@
 """Ray utilities."""
 
+import math
+import random
 from more_itertools import chunked
 
 import ray
@@ -10,6 +12,22 @@ from dask import config as dask_config
 
 def setup_ray_on_dask():
     dask_config.set(scheduler=ray_dask_get)
+
+
+def compute_chunksize(
+    kwargs_list: list[dict],
+    num_workers: int | None,
+    over_decomp_factor: int,
+) -> int:
+    if num_workers is None:
+        return 1
+
+    kwargs_list_len = len(kwargs_list)
+
+    chunksize = kwargs_list_len / (num_workers * over_decomp_factor)
+    chunksize = int(math.ceil(chunksize))
+    chunksize = max(1, chunksize)
+    return chunksize
 
 
 @ray.remote
@@ -23,9 +41,18 @@ def _ray_map_kwargs_chunk(func, kwargs_list_chunk):
 def ray_map_kwargs(
     func,
     kwargs_list: list[dict],
-    chunksize: int,
+    chunksize: int | None = None,
+    num_workers: int | None = None,
+    over_decomp_factor: int = 10,
+    shuffle: bool = False,
     **ray_options_kwargs,
 ):
+    if chunksize is None:
+        chunksize = compute_chunksize(kwargs_list, num_workers, over_decomp_factor)
+
+    if shuffle:
+        random.shuffle(kwargs_list)
+
     tasks = []
     for idx, kwargs_list_chunk in enumerate(chunked(kwargs_list, chunksize)):
         name = f"map-{func.__name__}-chunk-{idx}"
@@ -52,9 +79,18 @@ def _ray_apply_kwargs_chunk(func, kwargs_list_chunk):
 def ray_apply_kwargs(
     func,
     kwargs_list: list[dict],
-    chunksize: int,
+    chunksize: int | None = None,
+    num_workers: int | None = None,
+    over_decomp_factor: int = 10,
+    shuffle: bool = False,
     **ray_options_kwargs,
 ):
+    if chunksize is None:
+        chunksize = compute_chunksize(kwargs_list, num_workers, over_decomp_factor)
+
+    if shuffle:
+        random.shuffle(kwargs_list)
+
     tasks = []
     for idx, kwargs_list_chunk in enumerate(chunked(kwargs_list, chunksize)):
         name = f"apply-{func.__name__}-chunk-{idx}"
