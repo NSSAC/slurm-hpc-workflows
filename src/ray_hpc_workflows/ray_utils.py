db0f136bb6d210b7c73a5f99c3b2859a0dda8404
Parantapa Bhattacharya <pb@parantapa.net>
2025-01-18 11:03:32 -0500

    Use ray store to share args list and extra args

--- src/ray_hpc_workflows/ray_utils.py
+++ src/ray_hpc_workflows/ray_utils.py
@@ -32,9 +32,10 @@ def compute_chunksize(
 
 
 @ray.remote
-def _ray_map_chunk(func, args_list_chunk, extra_args):
+def _ray_map_chunk(func, args_idx_chunk, args_list, extra_args):
     results = []
-    for args in args_list_chunk:
+    for idx in args_idx_chunk:
+        args = args_list[idx]
         if extra_args is not None:
             args = list(args)
             args.extend(extra_args)
@@ -55,14 +56,18 @@ def ray_map(
     if chunksize is None:
         chunksize = compute_chunksize(args_list, num_workers, over_decomp_factor)
 
+    args_list_ref = ray.put(args_list)
+    extra_args_ref = ray.put(extra_args)
+    args_idx_list = list(range(len(args_list)))
+
     if shuffle:
-        random.shuffle(args_list)
+        random.shuffle(args_idx_list)
 
     tasks = []
-    for idx, args_list_chunk in enumerate(chunked(args_list, chunksize)):
+    for idx, args_idx_chunk in enumerate(chunked(args_idx_list, chunksize)):
         name = f"map-{func.__name__}-chunk-{idx}"
-        task = _ray_map_chunk.options(name=name, **ray_options_kwargs)  # type: ignore
-        task = task.remote(func, args_list_chunk, extra_args)
+        task = _ray_map_chunk.options(name=name, **ray_options_kwargs)
+        task = task.remote(func, args_idx_chunk, args_list_ref, extra_args_ref)
         tasks.append(task)
     results = ray.get(tasks)
 
@@ -74,8 +79,9 @@ def ray_map(
 
 
 @ray.remote
-def _ray_apply_chunk(func, args_list_chunk, extra_args):
-    for args in args_list_chunk:
+def _ray_apply_chunk(func, args_idx_chunk, args_list, extra_args):
+    for idx in args_idx_chunk:
+        args = args_list[idx]
         if extra_args is not None:
             args = list(args)
             args.extend(extra_args)
@@ -95,14 +101,18 @@ def ray_apply(
     if chunksize is None:
         chunksize = compute_chunksize(args_list, num_workers, over_decomp_factor)
 
+    args_list_ref = ray.put(args_list)
+    extra_args_ref = ray.put(extra_args)
+    args_idx_list = list(range(len(args_list)))
+
     if shuffle:
-        random.shuffle(args_list)
+        random.shuffle(args_idx_list)
 
     tasks = []
-    for idx, args_list_chunk in enumerate(chunked(args_list, chunksize)):
+    for idx, args_idx_chunk in enumerate(chunked(args_idx_list, chunksize)):
         name = f"apply-{func.__name__}-chunk-{idx}"
-        task = _ray_apply_chunk.options(name=name, **ray_options_kwargs)  # type: ignore
-        task = task.remote(func, args_list_chunk, extra_args)
+        task = _ray_apply_chunk.options(name=name, **ray_options_kwargs)
+        task = task.remote(func, args_idx_chunk, args_list_ref, extra_args_ref)
         tasks.append(task)
 
     _ = ray.get(tasks)
