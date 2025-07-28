6c6bc94928c376d1d723b70dde42c69cd5b97af9
Parantapa Bhattacharya <pb@parantapa.net>
2025-07-28 09:44:02 -0400

    .

---
+++ src/ray_hpc_workflows/slurm_pilot.py
@@ -0,0 +1,534 @@
+"""Pilot workers for slurm."""
+
+import os
+import time
+import json
+import pickle
+import socket
+import logging
+import threading
+from uuid import uuid4
+from datetime import datetime
+from dataclasses import dataclass, field
+from collections import deque
+from pathlib import Path
+from concurrent.futures import ThreadPoolExecutor, Future
+
+import grpc
+import click
+import platformdirs
+import cloudpickle
+
+from .slurm_job_manager import (
+    get_running_jobids,
+    cancel_jobs,
+    submit_sbatch_job,
+    SlurmJob,
+)
+from .slurm_pilot_pb2 import (
+    Empty,
+    WorkerProcessID,
+    TaskDefn,
+    TaskAssignment,
+    TaskResult,
+)
+from .slurm_pilot_pb2_grpc import (
+    CoordinatorServicer,
+    CoordinatorStub,
+    add_CoordinatorServicer_to_server,
+)
+from .utils import data_address, find_setup_script, arbitrary_free_port
+from .templates import render_template
+
+INTER_SQUEUE_CALL_TIME_S: float = 5.0
+NEXT_TASK_RETRY_TIME_S: float = 1.0
+LOG_FORMAT: str = "%(asctime)s:%(name)s:%(levelname)s:%(message)s"
+LOG_LEVEL = logging.INFO
+
+
+class PilotProcess:
+    def __init__(self, type: str, name: str, server_address: str, work_dir: Path):
+        slurm_job_id = int(os.environ.get("SLURM_JOB_ID", 0))
+        hostname = socket.gethostname()
+        pid = os.getpid()
+        self._process_id = WorkerProcessID(
+            type=type,
+            name=name,
+            slurm_job_id=slurm_job_id,
+            hostname=hostname,
+            pid=pid,
+        )
+
+        self._server_address = server_address
+        self._work_dir = work_dir
+        self._exit_flag = False
+        self._logger = logging.getLogger("slurm_pilot.worker_process")
+
+        service_config_json = json.dumps(
+            {
+                "methodConfig": [
+                    {
+                        "name": [{}],
+                        "retryPolicy": {
+                            "maxAttempts": 100,
+                            "initialBackoff": "0.1s",
+                            "maxBackoff": "5s",
+                            "backoffMultiplier": 2,
+                            "retryableStatusCodes": ["UNAVAILABLE"],
+                        },
+                    }
+                ]
+            }
+        )
+        options = [
+            # Keep alive stuff
+            ("grpc.keepalive_time_ms", 8000),
+            ("grpc.keepalive_timeout_ms", 5000),
+            ("grpc.http2.max_pings_without_data", 5),
+            ("grpc.keepalive_permit_without_calls", 1),
+            # Retry stuff
+            ("grpc.enable_retries", 1),
+            ("grpc.service_config", service_config_json),
+        ]
+
+        self._channel = grpc.insecure_channel(self._server_address, options=options)
+        self._stub = CoordinatorStub(self._channel)
+
+    def close(self):
+        self._channel.close()
+
+    def RegisterWorkerProcess(self) -> None:
+        self._stub.RegisterWorkerProcess(self._process_id)
+
+    def UnregisterWorkerProcess(self) -> None:
+        self._stub.UnregisterWorkerProcess(self._process_id)
+
+    def GetNextTask(self) -> TaskDefn | None:
+        assignment: TaskAssignment = self._stub.GetNextTask(self._process_id)
+        if assignment.exit_flag:
+            self._exit_flag = True
+            return None
+        elif assignment.task_available:
+            return assignment.task
+        return None
+
+    def SetTaskResult(self, result: TaskResult) -> None:
+        self._stub.SetTaskResult(result)
+
+    def do_run_task(self, task: TaskDefn) -> TaskResult:
+        try:
+            function = cloudpickle.loads(task.function)
+            args = cloudpickle.loads(task.args)
+            kwargs = cloudpickle.loads(task.args)
+
+            return_ = function(*args, **kwargs)
+            return_ = cloudpickle.dumps(return_)
+            result = TaskResult(
+                task_id=task.task_id,
+                task_success=True,
+                return_=return_,
+                process_id=self._process_id,
+            )
+        except Exception as e:
+            eid = str(uuid4())
+            self._logger.exception(
+                "Error executing task=%s: %s: %s", task.task_id, eid, e
+            )
+            result = TaskResult(
+                task_id=task.task_id,
+                task_success=False,
+                error=f"{type(e)}: {e}",
+                error_id=eid,
+                process_id=self._process_id,
+            )
+
+        return result
+
+    def main(self):
+        self.RegisterWorkerProcess()
+        try:
+            while True:
+                task = self.GetNextTask()
+                if task is not None:
+                    result = self.do_run_task(task)
+                    self.SetTaskResult(result)
+
+                if self._exit_flag:
+                    return
+                else:
+                    time.sleep(NEXT_TASK_RETRY_TIME_S)
+        finally:
+            self.UnregisterWorkerProcess()
+            self.close()
+
+
+@dataclass
+class TaskDefnWithFut:
+    defn: TaskDefn
+    fut: Future
+
+
+@dataclass
+class WorkerProcess:
+    key: str
+    process_id: WorkerProcessID
+    running_task: TaskDefnWithFut | None = None
+
+
+@dataclass
+class Worker:
+    name: str
+    slurm_job: SlurmJob
+    exit_flag: bool = False
+    is_running: bool = True
+    processes: dict[str, WorkerProcess] = field(default_factory=dict)
+
+    def has_running_tasks(self) -> bool:
+        return any(p.running_task is not None for p in self.processes.values())
+
+
+@dataclass
+class WorkerGroup:
+    type: str
+    sbatch_args: list[str]
+    is_batch_worker: bool
+    workers: dict[str, Worker] = field(default_factory=dict)
+    next_worker_index: int = 0
+    task_queue: deque[TaskDefnWithFut] = field(default_factory=deque)
+
+    # Available workers: workers with exit_flag == False
+    def available_workers(self) -> int:
+        return sum(1 for w in self.workers.values() if w.exit_flag)
+
+
+class PilotCoordinator(CoordinatorServicer):
+    def __init__(
+        self,
+        work_dir: Path | str | None = None,
+        setup_script: Path | str | None = None,
+    ):
+        self.setup_script = find_setup_script(setup_script)
+
+        if work_dir is None:
+            now = datetime.now().isoformat()
+            work_dir = platformdirs.user_cache_path(
+                appname=f"slurm-pilot-work-dir-{now}"
+            )
+        self.work_dir = Path(work_dir)
+        self.work_dir.mkdir(parents=True, exist_ok=True)
+
+        self._logger = logging.getLogger("pilot_coordinator")
+        self._lock = threading.Lock()
+        self._groups: dict[str, WorkerGroup] = {}
+        self._exit_flag = threading.Event()
+        self._queue_monitor_thread: threading.Thread | None = None
+        self._server_address: str | None = None
+        self._server: grpc.Server | None = None
+
+    def _cleanup_process(
+        self, group: WorkerGroup, worker: Worker, process: WorkerProcess
+    ):
+        if process.running_task is not None:
+            group.task_queue.appendleft(process.running_task)
+            process.running_task = None
+        del worker.processes[process.key]
+
+    def _cleanup_worker(self, group: WorkerGroup, worker: Worker):
+        for key in list(worker.processes):
+            process = worker.processes[key]
+            self._cleanup_process(group, worker, process)
+        del group.workers[worker.name]
+
+    def _cleanup_finished_workers(self):
+        try:
+            job_ids = get_running_jobids()
+        except Exception:
+            self._logger.exception("Failed to get running slurm job ids")
+            job_ids = None
+
+        if job_ids is not None:
+            with self._lock:
+                for group in self._groups.values():
+                    for name in list(group.workers):
+                        worker = group.workers[name]
+                        if not worker.slurm_job.job_id in job_ids:
+                            self._cleanup_worker(group, worker)
+
+    def _cleanup_all_workers(self):
+        try:
+            job_ids = get_running_jobids()
+        except Exception:
+            self._logger.exception("Failed to get running slurm job ids")
+            job_ids = None
+
+        still_running_job_ids: list[int] = []
+        if job_ids is not None:
+            with self._lock:
+                for group in self._groups.values():
+                    for name in list(group.workers):
+                        worker = group.workers[name]
+                        if worker.slurm_job.job_id in job_ids:
+                            still_running_job_ids.append(worker.slurm_job.job_id)
+                        self._cleanup_worker(group, worker)
+
+        if still_running_job_ids:
+            cancel_jobs(still_running_job_ids)
+
+    def _queue_monitor_main(self):
+        while True:
+            self._cleanup_finished_workers()
+
+            if self._exit_flag.wait(timeout=INTER_SQUEUE_CALL_TIME_S):
+                break
+
+    def define_worker(
+        self, type: str, sbatch_args: list[str], is_batch_worker: bool = False
+    ):
+        with self._lock:
+            group = WorkerGroup(
+                type=type, sbatch_args=sbatch_args, is_batch_worker=is_batch_worker
+            )
+
+            if group.type in self._groups:
+                assert self._groups[group.type] == group
+            else:
+                self._groups[group.type] = group
+
+    def _add_worker(self, group: WorkerGroup) -> None:
+        assert self._server_address is not None
+
+        worker_index = group.next_worker_index
+        group.next_worker_index += 1
+        name = f"slurm_pilot_worker.{type}.{worker_index}"
+
+        worker_script = render_template(
+            "slurm_pilot:worker_script",
+            type=type,
+            name=name,
+            server_address=self._server_address,
+            work_dir=str(self.work_dir),
+            setup_script=str(self.setup_script),
+        )
+        worker_script_path = self.work_dir / f"{name}.sh"
+        worker_script_path.write_text(worker_script)
+        worker_script_path.chmod(0o755)
+
+        worker_sbatch_script = render_template(
+            "slurm_pilot:worker_sbatch_script",
+            is_batch_worker=group.is_batch_worker,
+            worker_script_path=worker_script_path,
+        )
+
+        self._logger.info(f"Starting worker %s", name)
+        slurm_job = submit_sbatch_job(
+            name=name,
+            sbatch_args=group.sbatch_args,
+            script=worker_sbatch_script,
+            work_dir=self.work_dir,
+        )
+        group.workers[name] = Worker(name=name, slurm_job=slurm_job)
+
+    def scale_workers(self, type: str, count: int):
+        with self._lock:
+            group = self._groups[type]
+            if len(group.workers) < count:
+                to_hire = count - len(group.workers)
+                for _ in range(to_hire):
+                    self._add_worker(group)
+
+            if len(group.workers) > count:
+                to_retire = count - len(group.workers)
+
+                # First we try to retire
+                # without running tasks
+                for worker in group.workers.values():
+                    if (
+                        to_retire > 0
+                        and not worker.exit_flag
+                        and not worker.has_running_tasks()
+                    ):
+                        worker.exit_flag = True
+                        to_retire -= 1
+
+                # Next we retire active workers
+                for worker in group.workers.values():
+                    if to_retire > 0 and not worker.exit_flag:
+                        worker.exit_flag = True
+                        to_retire -= 1
+
+    def submit(self, type: str, fn, *args, **kwargs) -> Future:
+        defn = TaskDefn(
+            task_id=str(uuid4()),
+            function=cloudpickle.dumps(fn, protocol=pickle.HIGHEST_PROTOCOL),
+            args=cloudpickle.dumps(args, protocol=pickle.HIGHEST_PROTOCOL),
+            kwargs=cloudpickle.dumps(kwargs, protocol=pickle.HIGHEST_PROTOCOL),
+            type=type,
+        )
+        task = TaskDefnWithFut(defn=defn, fut=Future())
+
+        with self._lock:
+            group = self._groups[type]
+            group.task_queue.append(task)
+
+        return task.fut
+
+    def RegisterWorkerProcess(
+        self, request: WorkerProcessID, context: grpc.ServicerContext
+    ) -> Empty:
+        try:
+            with self._lock:
+                group = self._groups[request.type]
+                worker = group.workers[request.name]
+                process_key = f"{request.slurm_job_id}:{request.hostname}:{request.pid}"
+                process = WorkerProcess(
+                    key=process_key,
+                    process_id=request,
+                )
+
+                if process_key in worker.processes:
+                    assert worker.processes[process_key] == process
+                else:
+                    worker.processes[process_key] = process
+
+                return Empty()
+        except Exception as e:
+            eid = str(uuid4())
+            self._logger.exception("Unexpected exception: %s: %s", eid, e)
+            context.set_code(grpc.StatusCode.UNKNOWN)
+            context.set_details("Unexpected exception: %s: %s" % (eid, e))
+            raise grpc.RpcError(context)
+
+    def UnregisterWorkerProcess(
+        self, request: WorkerProcessID, context: grpc.ServicerContext
+    ) -> Empty:
+        try:
+            with self._lock:
+                group = self._groups[request.type]
+                worker = group.workers[request.name]
+                process_key = f"{request.slurm_job_id}:{request.hostname}:{request.pid}"
+                process = worker.processes[process_key]
+                self._cleanup_process(group, worker, process)
+
+                return Empty()
+        except Exception as e:
+            eid = str(uuid4())
+            self._logger.exception("Unexpected exception: %s: %s", eid, e)
+            context.set_code(grpc.StatusCode.UNKNOWN)
+            context.set_details("Unexpected exception: %s: %s" % (eid, e))
+            raise grpc.RpcError(context)
+
+    def GetNextTask(
+        self, request: WorkerProcessID, context: grpc.ServicerContext
+    ) -> TaskAssignment:
+        try:
+            with self._lock:
+                group = self._groups[request.type]
+                worker = group.workers[request.name]
+                process_key = f"{request.slurm_job_id}:{request.hostname}:{request.pid}"
+                process = worker.processes[process_key]
+                assert process.running_task is None
+
+                if group.task_queue and not worker.exit_flag:
+                    task = group.task_queue.popleft()
+                    process.running_task = task
+                    return TaskAssignment(
+                        exit_flag=worker.exit_flag, task_available=True, task=task.defn
+                    )
+                else:
+                    return TaskAssignment(
+                        exit_flag=worker.exit_flag,
+                        task_available=False,
+                    )
+        except Exception as e:
+            eid = str(uuid4())
+            self._logger.exception("Unexpected exception: %s: %s", eid, e)
+            context.set_code(grpc.StatusCode.UNKNOWN)
+            context.set_details("Unexpected exception: %s: %s" % (eid, e))
+            raise grpc.RpcError(context)
+
+    def SetTaskResult(
+        self, request: TaskResult, context: grpc.ServicerContext
+    ) -> Empty:
+        try:
+            with self._lock:
+                group = self._groups[request.process_id.type]
+                worker = group.workers[request.process_id.name]
+                process_key = f"{request.process_id.slurm_job_id}:{request.process_id.hostname}:{request.process_id.pid}"
+                process = worker.processes[process_key]
+                assert process.running_task is not None
+
+                if request.task_success:
+                    process.running_task.fut.set_result(
+                        cloudpickle.loads(request.return_)
+                    )
+                else:
+                    err = RuntimeError(
+                        "Error running task: %s: %s" % (request.error, request.error_id)
+                    )
+                    process.running_task.fut.set_exception(err)
+                process.running_task = None
+
+            return Empty()
+        except Exception as e:
+            eid = str(uuid4())
+            self._logger.exception("Unexpected exception: %s: %s", eid, e)
+            context.set_code(grpc.StatusCode.UNKNOWN)
+            context.set_details("Unexpected exception: %s: %s" % (eid, e))
+            raise grpc.RpcError(context)
+
+    def start(self):
+        assert self._queue_monitor_thread is None
+        assert self._server is None
+
+        self._exit_flag.clear()
+        self._queue_monitor_thread = threading.Thread(target=self._queue_monitor_main)
+
+        host = data_address(None)
+        port = arbitrary_free_port(host)
+
+        options = [
+            ("grpc.keepalive_time_ms", 20000),
+            ("grpc.keepalive_timeout_ms", 10000),
+            ("grpc.http2.min_ping_interval_without_data_ms", 5000),
+            ("grpc.max_connection_idle_ms", 10000),
+            ("grpc.max_connection_age_ms", 30000),
+            ("grpc.max_connection_age_grace_ms", 5000),
+            ("grpc.http2.max_pings_without_data", 5),
+            ("grpc.keepalive_permit_without_calls", 1),
+        ]
+
+        self._server_address = f"{host}:{port}"
+        self._server = grpc.server(ThreadPoolExecutor(max_workers=1), options=options)
+        add_CoordinatorServicer_to_server(self, self.server)
+        self._server.add_insecure_port(self._server_address)
+        self._server.start()
+
+    def close(self):
+        if self.server is not None:
+            self.server.stop(None)
+            self.server = None
+
+        if self._queue_monitor_thread is not None:
+            self._exit_flag.set()
+            self._queue_monitor_thread.join()
+            self._queue_monitor_thread = None
+
+            self._cleanup_all_workers()
+
+
+@click.command()
+@click.option("--type", type=str, required=True, help="Worker type")
+@click.option("--name", type=str, required=True, help="Worker job ID")
+@click.option("--server-address", type=str, required=True, help="Pilot server address")
+@click.option(
+    "--work-dir",
+    type=click.Path(exists=True, file_okay=False, dir_okay=True, path_type=Path),
+    required=True,
+    help="Work directory",
+)
+def slurm_pilot_process(type: str, name: str, server_address: str, work_dir: Path):
+    """Start a slurm pilot worker."""
+    worker = PilotProcess(
+        type=type, name=name, server_address=server_address, work_dir=work_dir
+    )
+    worker.main()
---
+++ src/ray_hpc_workflows/slurm_pilot_pb2.pyi
@@ -0,0 +1,64 @@
+from google.protobuf import descriptor as _descriptor
+from google.protobuf import message as _message
+from collections.abc import Mapping as _Mapping
+from typing import ClassVar as _ClassVar, Optional as _Optional, Union as _Union
+
+DESCRIPTOR: _descriptor.FileDescriptor
+
+class Empty(_message.Message):
+    __slots__ = ()
+    def __init__(self) -> None: ...
+
+class WorkerProcessID(_message.Message):
+    __slots__ = ("type", "name", "slurm_job_id", "hostname", "pid")
+    TYPE_FIELD_NUMBER: _ClassVar[int]
+    NAME_FIELD_NUMBER: _ClassVar[int]
+    SLURM_JOB_ID_FIELD_NUMBER: _ClassVar[int]
+    HOSTNAME_FIELD_NUMBER: _ClassVar[int]
+    PID_FIELD_NUMBER: _ClassVar[int]
+    type: str
+    name: str
+    slurm_job_id: int
+    hostname: str
+    pid: int
+    def __init__(self, type: _Optional[str] = ..., name: _Optional[str] = ..., slurm_job_id: _Optional[int] = ..., hostname: _Optional[str] = ..., pid: _Optional[int] = ...) -> None: ...
+
+class TaskDefn(_message.Message):
+    __slots__ = ("task_id", "function", "args", "kwargs", "type")
+    TASK_ID_FIELD_NUMBER: _ClassVar[int]
+    FUNCTION_FIELD_NUMBER: _ClassVar[int]
+    ARGS_FIELD_NUMBER: _ClassVar[int]
+    KWARGS_FIELD_NUMBER: _ClassVar[int]
+    TYPE_FIELD_NUMBER: _ClassVar[int]
+    task_id: str
+    function: bytes
+    args: bytes
+    kwargs: bytes
+    type: str
+    def __init__(self, task_id: _Optional[str] = ..., function: _Optional[bytes] = ..., args: _Optional[bytes] = ..., kwargs: _Optional[bytes] = ..., type: _Optional[str] = ...) -> None: ...
+
+class TaskAssignment(_message.Message):
+    __slots__ = ("exit_flag", "task_available", "task")
+    EXIT_FLAG_FIELD_NUMBER: _ClassVar[int]
+    TASK_AVAILABLE_FIELD_NUMBER: _ClassVar[int]
+    TASK_FIELD_NUMBER: _ClassVar[int]
+    exit_flag: bool
+    task_available: bool
+    task: TaskDefn
+    def __init__(self, exit_flag: bool = ..., task_available: bool = ..., task: _Optional[_Union[TaskDefn, _Mapping]] = ...) -> None: ...
+
+class TaskResult(_message.Message):
+    __slots__ = ("task_id", "task_success", "return_", "error", "error_id", "process_id")
+    TASK_ID_FIELD_NUMBER: _ClassVar[int]
+    TASK_SUCCESS_FIELD_NUMBER: _ClassVar[int]
+    RETURN__FIELD_NUMBER: _ClassVar[int]
+    ERROR_FIELD_NUMBER: _ClassVar[int]
+    ERROR_ID_FIELD_NUMBER: _ClassVar[int]
+    PROCESS_ID_FIELD_NUMBER: _ClassVar[int]
+    task_id: str
+    task_success: bool
+    return_: bytes
+    error: str
+    error_id: str
+    process_id: WorkerProcessID
+    def __init__(self, task_id: _Optional[str] = ..., task_success: bool = ..., return_: _Optional[bytes] = ..., error: _Optional[str] = ..., error_id: _Optional[str] = ..., process_id: _Optional[_Union[WorkerProcessID, _Mapping]] = ...) -> None: ...
