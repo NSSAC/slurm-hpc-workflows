60289cdb751f3fb6dbdc2dd0fbaa947c23c81ce5
Parantapa Bhattacharya <pb@parantapa.net>
2025-07-28 20:44:22 -0400

    Fix run jupyter

--- src/ray_hpc_workflows/run_jupyter.py
+++ src/ray_hpc_workflows/run_jupyter.py
@@ -1,13 +1,15 @@
 """Start a Jupyter Lab instance."""
 
 import subprocess
+from datetime import datetime
 from pathlib import Path
 
 import click
+import platformdirs
 
 from .utils import find_jupyter, find_setup_script
 from .templates import render_template
-from .slurm_job_manager import SlurmJobManager
+from .slurm_job_manager import submit_sbatch_job
 
 
 @click.command()
@@ -35,9 +37,14 @@ def run_jupyter(
         jupyter_executable=jupyter_executable,
     )
 
-    sjm = SlurmJobManager(cancel_on_close=False)
+    now = datetime.now().isoformat()
+    work_dir = platformdirs.user_cache_path(appname=f"run-jupyter-{now}")
+    work_dir.mkdir(parents=True)
+
     try:
-        job = sjm.submit(name=name, sbatch_args=sbatch_args, script=script)
+        job = submit_sbatch_job(
+            name=name, sbatch_args=sbatch_args, script=script, work_dir=work_dir
+        )
 
         print(f"Job ID: {job.job_id}")
         print(f"Output file: {job.output_file!s}")
